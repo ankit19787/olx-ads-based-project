88a0bea36aa5f48862f3e3adafde2b2cb651359e
ankit-bansal-bs <ankit.bansal@bigsteptech.com>
2015-09-26 16:53:40 +0530

    Display Ads

--- inc/twitteroauth.php
+++ inc/twitteroauth.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * Abraham Williams (abraham@abrah.am) http://abrah.am
- *
- * The first PHP Library to support OAuth for Twitter's REST API.
- */
-
 /* Load OAuth lib. You can find it at http://oauth.net */
 require_once('OAuth.php');
 
