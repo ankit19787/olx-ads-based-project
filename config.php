f84d1d5be99cfec87ef94bbdb24b5be3750c6104
ankit-bansal-bs <ankit.bansal@bigsteptech.com>
2015-09-26 16:41:36 +0530

    Display Ads

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 define('CONSUMER_KEY', '7JA6H3w24hYfOUXuQoHDWQ');
 define('CONSUMER_SECRET', 'AY3G0zbdTdzWdGS548wNfPzPsLVGF66XVoV9Kmfqs');
-define('OAUTH_CALLBACK', 'http://localhost/twitterapp/process.php');
+define('OAUTH_CALLBACK', 'http://localhost/olxadbasedapp/process.php');
 ?>
