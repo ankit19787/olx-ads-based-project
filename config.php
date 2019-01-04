9e63197bc8a7295f956bd77b4b60054e47588a44
ankit19787 <ankit19787@gmail.com>
2019-01-04 21:27:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
-
 define('CONSUMER_KEY', '7JA6H3w24hYfOUXuQoHDWQ');
 define('CONSUMER_SECRET', 'AY3G0zbdTdzWdGS548wNfPzPsLVGF66XVoV9Kmfqs');
 define('OAUTH_CALLBACK', 'http://localhost/olxadbasedapp/process.php');
